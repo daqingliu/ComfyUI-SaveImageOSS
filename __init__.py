e1c2dc3b6389b3cfa2f04ec7f506f9fb6f660df4
liudaqing1 <liudaqing1@jd.com>
2024-12-09 01:17:14 +0800

    add load from url

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
-from .nodes import SaveImageOSS
+from .nodes import SaveImageOSS, LoadImageOSS
 
 NODE_CLASS_MAPPINGS = {
     "SaveImageOSS": SaveImageOSS,
+    "LoadImageOSS": LoadImageOSS,
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS"]
